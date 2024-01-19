2a8993401064635ef660abc21a4783f7baeb76bf
Vineeta1628 <119848110+Vineeta1628@users.noreply.github.com>
2024-01-19 11:19:57 +0530

    Delete git and aws deployment directory

--- git and aws deployment/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# You can perform additional setup or imports here if needed
-# For example, you might import routes or configure extensions.
-
-# Import your routes to make them accessible
-from app import route
